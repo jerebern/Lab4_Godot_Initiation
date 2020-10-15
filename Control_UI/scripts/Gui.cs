ad83b5d203cad8af791cd5fa03658dbb4288583a
Jeremy Bernard <jere.bern@hotmail.com>
2020-10-15 18:05:47 -0400

    Part 3 1/2

---
+++ Control_UI/scripts/Gui.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public class Gui : MarginContainer
+{
+	private Label _numberLabel;
+	private TextureProgress _bar;
+	private Tween _tween;
+
+	public override void _Ready()
+	{
+		_bar = (TextureProgress) GetNode("Bars/LifeBar/TextureProgress");
+		_tween = (Tween) GetNode("Tween");
+		_numberLabel = (Label) GetNode("Bars/LifeBar/Count/Background/Number");
+		var player = (Player) GetNode("../Characters/Player");
+			_bar.MaxValue = player.MaxHealth;
+	}
+private void _on_Player_HealthChanged(int health)
+{
+	UpdateHealth(health);
+	// Replace with function body.
+}
+public void UpdateHealth(int health)
+{
+	_numberLabel.Text = health.ToString();
+	_bar.Value = health;
+}	
+	
+}
